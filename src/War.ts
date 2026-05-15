cd609e94efa0a0778f9dc2cf8a3b84ab51a71bb3
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-05-15 20:51:08 +0200

    feat(War): Resolve the addViking and addSaxon funcion

--- src/War.ts
+++ src/War.ts
@@ -5,8 +5,19 @@ import { Saxon } from "./Saxon";
 export class War {
     vikingArmy: Viking[];
     saxonArmy: Saxon[];
+    
     constructor(){
         this.vikingArmy = [];
         this.saxonArmy = [];
     }
+
+    addViking(viking: Viking){
+        this.vikingArmy.push(viking);
+    }
+
+    addSaxon(saxon: Saxon){
+        this.saxonArmy.push(saxon);
+
+    }
+
 }
