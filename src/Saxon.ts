5d3a7a7b0e3a82bffa844f039d0d22fd03a65f5f
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-05-15 19:53:59 +0200

    feat(Saxon): Resolve constructor function of soldier Saxon

--- src/Saxon.ts
+++ src/Saxon.ts
@@ -1,1 +1,8 @@
-export class Saxon{}
+import { Soldier } from "./Soldier";
+
+export class Saxon extends Soldier{
+    constructor(health: number, strength: number){
+        super(health, strength);
+
+    }
+}
