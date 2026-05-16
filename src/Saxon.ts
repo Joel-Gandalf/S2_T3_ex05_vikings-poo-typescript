affc66a91afba8303c016520dc7520fbcf4db245
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-05-16 13:29:34 +0200

    refactor(Saxon): Add typing for function returns

--- src/Saxon.ts
+++ src/Saxon.ts
@@ -1,14 +1,14 @@
 import { Soldier } from "./Soldier";
 
-export class Saxon extends Soldier{
-    constructor(health: number, strength: number){
+export class Saxon extends Soldier {
+    constructor(health: number, strength: number) {
         super(health, strength);
 
     }
-        receiveDamage(damge: number) {
+    receiveDamage(damge: number): string {
         super.receiveDamage(damge);
 
-        if(this.health <= 0){
+        if (this.health <= 0) {
             return 'A Saxon has died in combat';
         }
         return 'A Saxon has received ' + damge + ' points of damage';
